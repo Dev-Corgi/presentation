4f6d90fdcaceb863d7556ee1dca9428fb5dcab17
Dev-Corgi <bora5242@gmail.com>
2023-08-31 10:47:50 +0900

    add condition

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,26 @@
-import Image from 'next/image'
+"use client"
+import React,{useState,useEffect} from 'react';
+// import Image from 'next/image'
 import Dictionary1 from './pages/Dictionary1/page'
-import Gallary from './pages/Gallary/page'
+// import Gallary from './pages/Gallary/page'
 // import ScrollPage from './pages/ScrollPage/page'
-import ScrollPage from './pages/ScrollPage/page'
+// import ScrollPage from './pages/ScrollPage/page'
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true); // Add loading state
+
+  useEffect(() => {
+    setIsLoading(false); // Mark loading as false when done
+  }, []);
+
   return (
-   <Dictionary1></Dictionary1>
-  //  <Dictionary2></Dictionary2>
+
+    <>
+    {isLoading ? null :    <Dictionary1></Dictionary1>
+      //  <Dictionary2></Dictionary2>
       //  <Gallary></Gallary>
       // <ScrollPage></ScrollPage>
+    } 
+  </>
+
   )
 }
